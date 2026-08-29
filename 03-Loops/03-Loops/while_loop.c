5dc2142612aab216e0bf6fd26cad50a983dcea2d
toodler <toodlermeow@gmail.com>
2026-08-29 15:03:04 +0530

    Add while loop program

---
+++ 03-Loops/03-Loops/while_loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+
+    printf("Numbers from 1 to 10:\n");
+
+    while (i <= 10) {
+        printf("%d\n", i);
+        i++;
+    }
+
+    return 0;
+}
