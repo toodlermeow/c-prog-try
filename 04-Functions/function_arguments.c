e2a05fe57257da348baa492433b9bba0d13c6478
toodler <toodlermeow@gmail.com>
2026-08-29 15:12:11 +0530

    Add function to calculate sum of two numbers

---
+++ 04-Functions/function_arguments.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int num1, num2, sum;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    sum = add(num1, num2);
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
